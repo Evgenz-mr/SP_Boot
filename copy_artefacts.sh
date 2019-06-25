fd1c9658e35443de8f9c5ffb8fc319a3f301ec74
Evgeny_Mrykhin <evgeny_mrykhin@epam.com>
2019-06-25 14:17:12 +0300

    add copy_artefact

---
+++ copy_artefacts.sh
@@ -0,0 +1,6 @@
+cd /initial/build/libs/
+if [ ! -f /home/gradle/initial/build/libs/*.jar ]
+then
+    echo "=======COPY_ARTEFACT_TO_HOST========"
+fi
+scp -P 2222 *.jar root@192.168.145.164:~/12/
--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ if [ ! -f /home/gradle/*.jar ]
 then
     echo "=======STARTING SPRINT_BOOT========"
 fi
-java -jar build/libs/*.jar  
+java -jar home/gradle/*.jar  
