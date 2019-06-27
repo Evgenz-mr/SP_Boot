cf863326059671a72c4ca78583c2fdf8e397c687
Evgeny_Mrykhin <evgeny_mrykhin@epam.com>
2019-06-27 12:43:26 +0300

    add script (copy_artefacts)

--- copy_artefacts.sh
+++ copy_artefacts.sh
@@ -1,6 +1,6 @@
-cd initial/build/libs/
 if [ ! -f initial/build/libs/ ]
 then
     echo "=======COPY_ARTEFACT_TO_HOST========"
 fi
+cd initial/build/libs/
 scp -P 2222 *.jar root@192.168.145.164:~/12/artefacts/
